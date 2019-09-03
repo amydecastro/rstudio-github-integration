8d21f1da084c6b73eddf5a49c353badb5e5b0d16
Amy DeCastro <decastro@cu-biotgw-3-10.219.132.53.int.colorado.edu>
2019-09-03 14:12:46 -0600

    getting the project ready for RStudio

---
+++ data/Data_carpentry/get-24-hr-mcd14-data-conus.R
@@ -0,0 +1,7 @@
+# Purpose: Read in MCD14 data
+
+library(tidyverse)
+library(sf)
+
+afd <- read.csv("https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_USA_contiguous_and_Hawaii_24h.csv")
+
